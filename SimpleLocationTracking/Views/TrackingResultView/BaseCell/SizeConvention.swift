42e6f2b939623108674c87eea4b054826b7df608
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2023-12-21 14:00:42 +0900

    Padding 관련 컨벤션 추가

---
+++ SimpleLocationTracking/Views/TrackingResultView/BaseCell/SizeConvention.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  SimpleLocationTracking
+//
+//  Created by TAEHYOUNG KIM on 12/21/23.
+//
+
+import UIKit
+
+var padding_title_body: CGFloat {
+    10
+}
+
+var padding_subTitle_body: CGFloat {
+    10
+}
+
+var padding_body_view: CGFloat {
+    16
+}
+
+var padding_body_body: CGFloat {
+    10
+}

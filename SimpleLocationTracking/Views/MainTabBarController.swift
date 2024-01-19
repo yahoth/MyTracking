eba07716c2bf05b1b60004e32e4c4d425ce99fa4
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-01-20 03:59:15 +0900

    [Add] tabbar title update 코드 추가

--- SimpleLocationTracking/Views/MainTabBarController.swift
+++ SimpleLocationTracking/Views/MainTabBarController.swift
@@ -11,14 +11,37 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
 
         let mainVC = TrackingSetupViewController()
-        mainVC.title = "Main"
         mainVC.tabBarItem = UITabBarItem(title: "Tracking", image: UIImage(systemName: "speedometer"), tag: 0)
 
         let historyVC = HistoryViewController()
         historyVC.tabBarItem = UITabBarItem(title: "History", image: UIImage(systemName: "filemenu.and.selection"), tag: 1)
 
         self.viewControllers = [mainVC, historyVC]
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        updateNavigationItem(vc: self.selectedViewController!)
+    }
+
+    func updateNavigationItem(vc: UIViewController) {
+        switch vc {
+        case is TrackingSetupViewController:
+            navigationController?.navigationBar.prefersLargeTitles = true
+            title = "Go Tracking"
+        case is HistoryViewController:
+            navigationController?.navigationBar.prefersLargeTitles = false
+            title = "Tracking History"
+        default:
+            break
+        }
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        updateNavigationItem(vc: viewController)
     }
 }

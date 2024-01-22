cacc6076e62ae4a0682d29523d94623572d667b2
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-01-22 23:46:07 +0900

    [Add] 위치 정보 허용여부에 따라 트래킹 뷰에 접근 가능하도록 하고, 허용 거절 시 alert 추가

--- SimpleLocationTracking/ViewModels/TrackingSetupViewModel.swift
+++ SimpleLocationTracking/ViewModels/TrackingSetupViewModel.swift
@@ -6,9 +6,54 @@
 //
 
 import Foundation
+import CoreLocation
+import Combine
 
 class TrackingSetupViewModel {
     let locationManager = LocationManager()
+    var subscriptions = Set<AnyCancellable>()
+    @Published var status: CLAuthorizationStatus?
 
-    
+    init() {
+        bind()
+    }
+
+    func bind() {
+        locationManager.$authorizationStatus
+            .sink { [weak self] status in
+                self?.status = status
+            }.store(in: &subscriptions)
+    }
+
+    func request(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestAuthorization()
+        case .restricted, .denied:
+            print("denied")
+            // alert present
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("authorized")
+            // present modal
+        @unknown default:
+            fatalError()
+        }
+    }
+
+    func goTrackingButtonTapped(authorized: () -> Void, denied: () -> Void) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestAuthorization()
+        case .restricted, .denied:
+            print("denied")
+            denied()
+            // alert present
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("authorized")
+            authorized()
+            // present modal
+        default:
+            print("hello")
+        }
+    }
 }
--- SimpleLocationTracking/Views/TrackingSetupViewController.swift
+++ SimpleLocationTracking/Views/TrackingSetupViewController.swift
@@ -11,16 +11,17 @@ import SnapKit
 
 class TrackingSetupViewController: UIViewController {
     var startTrackingButton: UIButton!
+    var vm: TrackingSetupViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-
+        vm = TrackingSetupViewModel()
         startTrackingButton = UIButton()
         startTrackingButton.setTitle("Go Tracking", for: .normal)
         startTrackingButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
         startTrackingButton.setTitleColor(.accent, for: .normal)
-        startTrackingButton.addTarget(self, action: #selector(start), for: .touchUpInside)
+        startTrackingButton.addTarget(self, action: #selector(goTrackingButtonTapped), for: .touchUpInside)
         startTrackingButton.layer.borderWidth = 10
         startTrackingButton.layer.borderColor = UIColor.accent.cgColor
         startTrackingButton.layer.cornerRadius = 50
@@ -31,7 +32,35 @@ class TrackingSetupViewController: UIViewController {
         }
     }
 
-    @objc func start() {
+    @objc func goTrackingButtonTapped() {
+
+        vm.goTrackingButtonTapped(authorized: startTracking, denied: alertWhenPermissionStatusIsRejected)
+    }
+
+    func alertWhenPermissionStatusIsRejected() {
+        let title = "위치 정보 권한 요청"
+        let message = "이 서비스는 위치 정보가 필요합니다. '확인'을 눌러 위치 정보 권한을 변경해주세요."
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        let settingsAction = UIAlertAction(title: "확인", style: .default) { (_) -> Void in
+            guard let appSettings = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+
+            if UIApplication.shared.canOpenURL(appSettings) {
+                UIApplication.shared.open(appSettings)
+            }
+        }
+
+        alert.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+
+
+
+    func startTracking() {
         let vc = TrackingViewController()
         vc.modalPresentationStyle = .fullScreen
         navigationController?.present(vc, animated: true)

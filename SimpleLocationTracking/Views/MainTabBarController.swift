2d2b1d7833c31e7d4d7f18ffdd27d2794fe27550
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-02-29 16:44:29 +0900

    [Clean] 임시 코드 제거

--- SimpleLocationTracking/Views/MainTabBarController.swift
+++ SimpleLocationTracking/Views/MainTabBarController.swift
@@ -35,41 +35,14 @@ class MainTabBarController: UITabBarController {
     func updateNavigationItem(vc: UIViewController) {
         switch vc {
         case is TrackingSetupViewController:
-//            navigationController?.navigationBar.prefersLargeTitles = true
-            navigationItem.rightBarButtonItems = []
             navigationController?.navigationBar.isHidden = true
         case is HistoryViewController:
             navigationController?.navigationBar.prefersLargeTitles = false
             title = "Tracking History"
-
-            let addItem = UIBarButtonItem(image: UIImage(systemName: "plus.app.fill"), style: .done, target: self, action: #selector(addItem))
-            navigationItem.rightBarButtonItems = [addItem]
             navigationController?.navigationBar.isHidden = false
-
-//        case is ChartTestViewController:
-//            navigationController?.navigationBar.prefersLargeTitles = false
-//            title = "Charts"
-//
-//            navigationItem.rightBarButtonItems = []
         default:
             break
         }
-    }
-
-    @objc func addItem() {
-//        var components = DateComponents()
-//        let calendar = Calendar.current
-//        let years = 2020...2025
-//        let months = 1...12
-//        let day = 1...31
-//        components.year = years.randomElement()
-//        components.month = months.randomElement()
-//        components.day = day.randomElement()
-//        let date = calendar.date(from: components)!
-//
-//        let item = TrackingData(speedInfos: [SpeedInfo()].toRealmList(), pathInfo: PathInfo(), startDate: date, endDate: Date(), startLocation: "\(components.year!)-\(components.month!)-\(components.day!)", endLocation: "hello", activityType: .airplane)
-//
-//        RealmManager.shared.create(object: item)
     }
 }
 

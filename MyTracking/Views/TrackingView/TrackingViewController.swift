c55b28fc867700a2b9ef62c7d235b1be957ba2fe
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-03-02 14:06:01 +0900

    [Fix] se 사용 시 보이는 맵뷰 트래킹 안되는 문제 수정

--- MyTracking/Views/TrackingView/TrackingViewController.swift
+++ MyTracking/Views/TrackingView/TrackingViewController.swift
@@ -53,6 +53,7 @@ class TrackingViewController: UIViewController, FloatingPanelControllerDelegate 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         vm.startAndPause()
+        mapView.userTrackingMode = .followWithHeading
     }
 
     func setConstraints() {
@@ -82,7 +83,6 @@ class TrackingViewController: UIViewController, FloatingPanelControllerDelegate 
     func setMapView() {
         mapView = MKMapView()
         mapView.showsCompass = true
-        mapView.userTrackingMode = .followWithHeading
         mapView.showsUserLocation = true
         mapView.delegate = self
         mapView.setContentCompressionResistancePriority(.required, for: .vertical)

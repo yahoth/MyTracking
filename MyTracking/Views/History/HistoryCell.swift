d6c9c29ed2bf036aa09a920e21de8241ea2ac26f
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-03-02 21:56:26 +0900

    [Design] 셀 컬러 변경

--- MyTracking/Views/History/HistoryCell.swift
+++ MyTracking/Views/History/HistoryCell.swift
@@ -29,7 +29,7 @@ class HistoryCell: UICollectionViewCell {
 
     var typeImageContainer: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .white
         return view
     }()
 

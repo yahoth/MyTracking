4987fdc73993a7a3969864a25b4163e1f7c4ed7e
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-03-08 20:20:25 +0900

    [Add] EditVC dismiss 버튼 추가

--- MyTracking/Views/TrackingResultView/TrackingResultRouteCell/EditViewController.swift
+++ MyTracking/Views/TrackingResultView/TrackingResultRouteCell/EditViewController.swift
@@ -34,11 +34,7 @@ class EditViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationItem.title = "Edit"
-        let doneItem = UIBarButtonItem(title: "Done".localized(), style: .done, target: self, action: #selector(done))
-        navigationItem.rightBarButtonItems = [doneItem]
-
-
+        setNavigationBarItems()
         setTextFieldText()
         setTextField()
         setConstraints()
@@ -55,12 +51,29 @@ class EditViewController: UIViewController {
         }
     }
 
-    @objc func done() {
-        RealmManager.shared.update(item: trackingData, start: startPlaceTextField.text, end: endPlaceTextField.text)
+    func setNavigationBarItems() {
+        navigationItem.title = "Edit"
+        let doneItem = UIBarButtonItem(title: "Done".localized(), style: .done, target: self, action: #selector(done))
+        navigationItem.rightBarButtonItems = [doneItem]
 
-        dismiss(animated: true) { [weak self] in
-            self?.editChanges?()
+        let dismissItem = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(dismissEditVC))
+        navigationItem.leftBarButtonItems = [dismissItem]
+    }
+
+    @objc func done() {
+        if trackingData.startLocation != startPlaceTextField.text || trackingData.endLocation != endPlaceTextField.text {
+            RealmManager.shared.update(item: trackingData, start: startPlaceTextField.text, end: endPlaceTextField.text)
+
+            dismiss(animated: true) { [weak self] in
+                self?.editChanges?()
+            }
+        } else {
+            dismiss(animated: true)
         }
+    }
+
+    @objc func dismissEditVC() {
+        dismiss(animated: true)
     }
 
     func setConstraints() {
@@ -79,41 +92,6 @@ class EditViewController: UIViewController {
 }
 
 extension EditViewController: UITextFieldDelegate {
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldEndEditing")
-        return true
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        print("textFieldDidEndEditing")
-    }
-
-    // 입력을 시작할때 호출 (시작할지 말지를 물어보는 것)
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldBeginEditing")
-//        routeLabelView.textfield1.becomeFirstResponder()
-        return true
-    }
-
-    // 입력이 시작되면 호출 (시점)
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("textFieldDidBeginEditing")
-    }
-
-    // 내용을 삭제할 때 호출 (삭제할지 말지를 물어보는 것) .clearButtonMode과 연관
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        print("textFieldShouldClear")
-
-        return true
-    }
-
-    // 한글자 한글자 입력/지워질때마다 호출 ---> true(입력 허락), false(입력 거부)
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print("textField")
-        return true
-    }
-
-    // 엔터키가 눌러졌을때 호출 (동작할지 말지 물어보는 것)
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if startPlaceTextField == textField {
             endPlaceTextField.becomeFirstResponder()

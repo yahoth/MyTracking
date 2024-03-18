982723111e3653650b7c12be8c34c87944063dbd
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-03-18 23:54:36 +0900

    [Add] History가 없을 때 보여줄 뷰 생성

---
+++ MyTracking/Views/History/EmptyHistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyHistoryView.swift
+//  MyTracking
+//
+//  Created by TAEHYOUNG KIM on 3/18/24.
+//
+
+import UIKit
+
+class EmptyHistoryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

a17b4aef2080309ced1d9a996f73675301350c5a
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-03-24 23:56:26 +0900

    [Add] Share.xib파일과 View 프로퍼티 연결 및 오토레이아웃 조절

--- MyTracking/Views/TrackingResultView/ShareView.swift
+++ MyTracking/Views/TrackingResultView/ShareView.swift
@@ -6,11 +6,25 @@
 //
 
 import UIKit
+import MapKit
 
 @IBDesignable
 class ShareView: UIView {
 
 
+    @IBOutlet weak var startLocation: UILabel!
+    @IBOutlet weak var time: UILabel!
+    
+    @IBOutlet weak var endLocation: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
 
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var averageSpeedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var topSpeedLabel: UILabel!
 
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var averageSpeed: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var topSpeed: UILabel!
 }
